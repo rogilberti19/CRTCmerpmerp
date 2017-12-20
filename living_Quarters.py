d7a5ec061dbc991b713a4a50d362e4f6d348e7b7
rogilberti19 <34578618+rogilberti19@users.noreply.github.com>
2017-12-20 10:56:22 -0500

    living_1/4_elevators.py

--- living_Quarters.py
+++ living_Quarters.py
@@ -1,6 +1,5 @@
-########################################################
-
 while True:
+    #intro to the room
     input("You are decending in an old elevator. Slowly, it creaks to a stop and the doors open.")
     input("You are greeted with the rancid smell of teen's sweat.")
     input("An errie white lights line the ceiling, a few flicker as you look on")
@@ -9,117 +8,20 @@ while True:
     input("There is a pillow at the head of each bed and a chest at the foot.")
     input("Everything is still, too still...")
     
-########################################################
+    #outro to the room
+x = raw_input("What would you like to do?  ")
+if x == (str("goto elevator")):
+    print("You are now in the elevator...")
+    in_elevator = True
 
-    inventory = [] 
-    input("The bed...")
-    input("Its pressence calls out to you.")
-    input("You notice that something silver underneath the pillow.")
-    while True:
-        investigate = input("Do you want to investigate?:")
-        if investigate.lower() == "yes":       
-            input("You found a KEY")
-            inventory.append("KEY")
-            break
-        elif investigate.lower() == "no":
-            break
+while b = True
+y = raw_input("What would you like to do?  ")
+if y == ("press up")and in_elevator:
+    print("The elevator doors close with a thud")
+    input("it slowly climbs its way back to the first floor")
+    input("however the doors stay closed")
+    k = False
+elif x == ("use elevator"):
+    input("what would you like to use on the elevator?")
 
-########################################################
-
-    trunk = ["NOTEBOOK", "PEN","DAGGER"]
-    if "KEY" in inventory: 
-        input("Wth the key in hand you approach the large wooden trunk.")
-        input("You insert the key hopefully into the old lock on the front.")
-        input("In the trunk you find a collection of NOTEBOOKS, PENS, a  SMALL DAGGER,")
-        inventory.extend(trunk) 
-    else:
-        input("This trunk requires a KEY")
-   
-########################################################
-
-    break
-    
-    
-    
-    
-# -*- coding: utf-8 -*-
-#beginning of encounter
-
-
-
-
-
-
-print("You encounter a giant fluffy crab")
-print("It seems angry for some reason")
-print("How will you defend yourself?")
-def crab():
-    #Making Crab
-    print(" /\\   @ @")
-    print("( /   | |    ()")
-    print(" \\  __| |__  / ")
-    print('  -/   "   \\-')
-    print(" /-|       |-\\")
-    print("/ /-\     /-\ \\")
-    print(" / /-`---'-\ \\")
-    print("  /         \\")
-#default for the health of the crab
-crabhealth = ["#", "#", "#",]
-def health():
-    #Displays the health of the crab
-    print("")
-    print("")
-    print("Crab health: " + str(crabhealth))
-
-p1health = ["#", "#", "#",]
-def phealth():
-    #Displays the health of the crab
-    print("")
-    print("")
-    print("Player health: " + str(p1health))
-
-health()
-crab()
-phealth()
-
-import tkinter as tk
-
-root = tk.Tk()
-
-
-
-def attack():
-    print("You swing your sword at the crab, hitting it derectly")
-    crabhealth.remove(crabhealth[-1])
-    if len(crabhealth) == 0:
-        end()  
-    else:
-        health()
-        crab()
-def end():
-    print("Your swift abilities have caused the crab to faint, well done")
-    root.destroy()
-
-
-
-def talk():
-    print("The crab does not seem to understand you and clacks its claws viciously")
-    health()
-    crab()
-def run():
-    print("You try to run away but the crab prevents you with some form of hypnotics")
-    health()
-    crab()
-    
-button = tk.Button(root, text='Attack', width=25, command=lambda: attack())
-button2 = tk.Button(root, text='Talk', width=25, command=lambda: talk())
-button3 = tk.Button(root, text='Run', width=25, command=lambda: run())
-#Note that without the lambda: in the command line it will do nothing
-
-
-#Don't forget to pack!
-button.pack()
-button2.pack()
-button3.pack()
-#Run the system
-root.mainloop()
+    continue
